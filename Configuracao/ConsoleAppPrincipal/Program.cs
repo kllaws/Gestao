fd18a5c631f74298f6966e11fc701d803be234df
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-02-09 20:09:57 -0300

    banco

--- Configuracao/ConsoleAppPrincipal/Program.cs
+++ Configuracao/ConsoleAppPrincipal/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace ConsoleAppPrincipal
+﻿using System;
+
+namespace ConsoleAppPrincipal
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x;
+            int y;
+            int z;
+
+            while(true)
+            {
+               
+                Console.WriteLine("informe o primeiro numero");
+                y = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("informe o segundo numero");
+                z = Convert.ToInt32(Console.ReadLine());
+
+                try
+                {
+                    x = y / z;
+                    Console.WriteLine( "O resultado e: "+ x);
+                    Console.WriteLine("\nprecione enter para continuar. ");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ocorreu um erro ao tentar");
+                }
+                
+                  
+
+            }
         }
     }
 }
