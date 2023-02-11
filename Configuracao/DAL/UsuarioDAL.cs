87f963c3c2c012036e597cabaacbf257178badf9
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-02-10 21:53:25 -0300

    Sucesso

--- Configuracao/DAL/UsuarioDAL.cs
+++ Configuracao/DAL/UsuarioDAL.cs
@@ -16,12 +16,26 @@ namespace DAL
 
                 cn.ConnectionString = Conexao.StringDeConexao;
                 SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = @"INSERT INTO Usuario(nome, NomeUsuario, CPF, Email, senha, ativos) 
+                                       VALUES(@Nome, @NomeuUsuario, @CPF, @Email, @Senha, @Ativo)";
 
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Nome",_usuario.Nome);
+                cmd.Parameters.AddWithValue("NomeUsuario", _usuario.NomeUsuario);
+                cmd.Parameters.AddWithValue("CPF", _usuario.Cpf);
+                cmd.Parameters.AddWithValue("Email", _usuario.Email);
+                cmd.Parameters.AddWithValue("Senha", _usuario.Senha);
+                cmd.Parameters.AddWithValue("Ativo", _usuario.Ativo);
+
+                cn.Open();
+                cmd.ExecuteScalar();
 
 
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                throw new Exception("Ocorreu um erro ao tentar inserir um usuario no banco: "+ ex.Message);
 
             }
             finally
