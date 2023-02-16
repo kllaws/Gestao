6ce862ba1fd74a447824b6689cd9c353fb5002d2
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-02-16 19:41:55 -0300

    up

--- Configuracao/DAL/PermissaoDAL.cs
+++ Configuracao/DAL/PermissaoDAL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,25 @@ namespace DAL
 {
     internal class PermissaoDAL
     {
+
+        public void Inserir(PermissaoDAL _permissaoDAL)
+        {
+
+        }
+        public PermissaoDAL Buscar(string _nomeUsuario)
+        {
+            return new PermissaoDAL();
+        }
+        public void Alterar(PermissaoDAL _usuario)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+
+
 
     }
 }
