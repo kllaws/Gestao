2b0b41de3b689e67372e42197b350e02b7e03db1
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-02-20 16:33:07 -0300

    alteração no codigo

--- Configuracao/DAL/PermissaoDAL.cs
+++ Configuracao/DAL/PermissaoDAL.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
 
 namespace DAL
 {
     internal class PermissaoDAL
     {
+        public void Alterar(Permissao _permissao)
+        {
+            SqlConnection cn = new SqlConnection();
+
+            try
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = @"UPDATE Permissao SET Descricao = @Descricao WHERE IdDescricao = @IdDescricao";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@IdDescricao", _permissao.IdDescricao);
+
+                cn.Open();
+                cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Ocorreu um erro ao tentar atualizar uma descrição no banco: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public void Excluir(Permissao _permissao)
+        {
+            SqlConnection cn = new SqlConnection();
+
+            try
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = @"DELETE FROM Permissao WHERE IdDescricao = @IdDescricao";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@IdDescricao", _permissao.IdDescricao);
+
+                cn.Open();
+                cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Ocorreu um erro ao tentar excluir uma descrição no banco: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
     }
 }
