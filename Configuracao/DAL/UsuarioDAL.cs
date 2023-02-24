e740d7f116f76e5066edabb8aba70bd5a5b13d0e
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-02-23 21:27:07 -0300

    atualizaçao de informaçoes

--- Configuracao/DAL/UsuarioDAL.cs
+++ Configuracao/DAL/UsuarioDAL.cs
@@ -1,10 +1,13 @@
 ﻿using Models;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
+using System.Diagnostics;
+
 namespace DAL
 {
     public class UsuarioDAL
     {
+       
         public void Inserir(Usuario _usuario)
         {
             SqlConnection cn = new SqlConnection();//cn é um objeto de conexao
@@ -34,9 +37,54 @@ namespace DAL
                 cn.Close();
             }
         }
-        public Usuario Buscar(string _nomeUsuario)
+        public Usuario BuscarPorNomeUsuario(string _nomeUsuario)
         {
             return new Usuario();//para solucionar o erro de vazio.
+        }
+        public static List<Usuario> BuscarTodos()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+            Usuario usuario;
+            SqlCommand cmd = new SqlCommand();
+            SqlConnection cn = new SqlConnection();
+            
+            try
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                cmd.Connection = cn;
+                cmd.CommandText = @"SELECT ID, NOME, CPF, Email, Ativo FROM Usuario";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                
+                {
+                    while (rd.Read())
+                    {
+                        usuario = new Usuario();
+                        usuario.Id = Convert.ToInt32(rd["id"]);
+                        usuario.Nome = rd["nome"].ToString();
+                        usuario.NomeUsuario = rd["NomeUsuario"].ToString();
+                        usuario.CPF = rd["CPF"].ToString();
+                        usuario.Email = rd["email"].ToString();
+                        usuario.Ativo = Convert.ToBoolean(rd["Ativo"]);
+
+                        usuario.Add(usuario);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(String.Format("Ocorreu o seguinte erro: {0} ao tentar buscar no banco, o numero do erro e {1}", ex.Message, 154));
+                Console.WriteLine($"Ocorreu o seguinte erro: {ex.Message} ao tentar buscar no banco o numero do erro");
+                throw new Exception("Ocorreu um erro ao tentar buscar todos os usuarios: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return usuarios;
         }
         public void Alterar(Usuario usuario)
         {
