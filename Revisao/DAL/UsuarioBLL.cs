0073808e0732cab0590b3a863bab8a1e1583f2f4
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-02-28 20:19:47 -0300

    ainda preciso corrigir isso

---
+++ Revisao/DAL/UsuarioBLL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class UsuarioBLL
+    {
+        public void Inserir(Usuario _usuario)
+        {
+            UsuarioBLL usuarioDAL = new UsuarioDAL();
+            usuarioDAL.Inserir(_usuario);
+
+        }
+    }
+}
