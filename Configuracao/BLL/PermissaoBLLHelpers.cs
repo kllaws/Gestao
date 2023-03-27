2ca1ad2331dc21dec2aea36acef4de2c9f1e7496
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-03-27 10:08:05 -0300

    SEM AUTERAÇOES

---
+++ Configuracao/BLL/PermissaoBLLHelpers.cs
@@ -0,0 +1,26 @@
+﻿using BLL;
+using ConsoleAppPrincipal;
+
+internal static class PermissaoBLLHelpers
+{
+        public static void Alterar(Permissao Permissao)
+        {
+            if (Permissao.Descricao.Length <= 30 || Permissao.Descricao.Length >= 250)
+            {
+                throw new Exception("A descrição deve ter entre 30 e 250 caracteres.");
+            }
+
+            PermissaoDAL permissaoDAL = new PermissaoDAL();
+            permissaoDAL.Alterar(Permissao);
+        }
+    public static void Inserir(Permissao Permissao)
+    {
+        if (Permissao.Descricao.Length <= 30 || Permissao.Descricao.Length >= 250)
+        {
+            throw new Exception("A descrição deve ter entre 30 e 250 caracteres.");
+        }
+
+        PermissaoDAL permissaoDAL = new PermissaoDAL();
+        permissaoDAL.Inserir(Permissao);
+    }
+}
