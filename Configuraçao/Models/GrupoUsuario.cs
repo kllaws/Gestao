ed3ae035f8eb782dc7ae06402dbbc124084c5a2f
Claudio Martins Da Silva <123429011+kllaws@users.noreply.github.com>
2023-03-31 22:02:38 -0300

    up

--- Configuraçao/Models/GrupoUsuario.cs
+++ Configuraçao/Models/GrupoUsuario.cs
@@ -11,5 +11,7 @@ namespace Models
         public string NomeGrupo { get; set; }
         public List<Permissao> Permissao { get; set; }
         public int IdGrupoUser { get; set; }
+        public int IdGrupoUsuario { get; set; }
+        public List<Permissao> Permissoes { get; set; }
     }
 }
